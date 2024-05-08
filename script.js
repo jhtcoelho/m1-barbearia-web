5be1865a8b135104e7a82425b85b88b20dec31ba
João Coelho <62582623+jhtcoelho@users.noreply.github.com>
2024-05-07 22:07:08 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -16,30 +16,78 @@ const barbearia = {
     ],
     estaAberto: true,
 };
-  
-  
+    
 function buscaCortePorId(id) {
+    for (let corte of barbearia.cortes){
+        if (corte.id === id){
+            return corte;
+        }
+    }
+    return 'Corte não encontrado';
 }
 
 function buscaBarbaPorId(id) {
+    for (let barba of barbearia.barbas){
+        if(barba.id === id){
+            return barba;
+        }
+    }
+    return 'Barba não encontrada';
 }
 
 function verificaStatusBarbearia() {
-}
+    if (barbearia.estaAberto) {
+      return "Estamos abertos";
+    } else {
+      return "Estamos fechados";
+    }
+  }
 
 function retornaTodosCortes() {
+    return barbearia.cortes;
 }
 
 function retornaTodasBarbas() {
+    return barbearia.barbas;
 }
 
 function criaPedido(nomeCliente, corteId, barbaId) {
-}
+    const corte = buscaCortePorId(corteId);
+    const barba = buscaBarbaPorId(barbaId);
+  
+    // Verifica se o corte e a barba foram encontrados
+    if (corte === "Corte não encontrado") {
+      console.error("Corte não encontrado");
+      return;
+    }
+  
+    if (barba === "Barba não encontrada") {
+      console.error("Barba não encontrada");
+      return;
+    }
+  
+    const pedido = {
+      nome: nomeCliente,
+      pedidoCorte: corte.tipo,
+      pedidoCortePreco: corte.valor,
+      pedidoBarba: barba.tipo,
+      pedidoBarbaPreco: barba.valor,
+    };
+  
+    return pedido;
+  }
 
 function atualizarServico(lista, id, valor, tipo) {
-}
-
+    for (let i = 0; i < lista.length; i++) {
+        if (lista[i].id === id) {
+            lista[i].tipo = tipo;
+            lista[i].valor = valor;
+            return lista; // Retorna a lista atualizada após encontrar o serviço com o ID correspondente
+        }
+    }
+    return lista; // Retorna a lista original se o serviço com o ID correspondente não for encontrado
+  } 
+  
 function calculaTotal(pedido) {
+    return pedido.pedidoCortePreco + pedido.pedidoBarbaPreco;
 }
-  
-  
